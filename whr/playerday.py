c9b4b3531082cdf157ea36cdf531de6b8045e632
rymuelle <33radar@gmail.com>
2021-01-18 13:26:56 -0600

    fixed log norm derivative issues

--- whr/playerday.py
+++ whr/playerday.py
@@ -37,30 +37,30 @@ class PlayerDay:
                     print(
                         f"other_gamma ({g.opponent(self.player).__str__()}) = {other_gamma}"
                     )
-                print(other_gamma)
                 mov = g.player_mov(self.player)
                 a_gamma = 10 ** (mov / self.scale)
                 b_gamma = 10 ** (-mov / self.scale)
                 sum_gamma = a_gamma + b_gamma
                 a_gamma = a_gamma/sum_gamma
                 b_gamma = b_gamma/sum_gamma
-                self._mov_game_terms.append([a_gamma, b_gamma, a_gamma, other_gamma])
+                self._mov_game_terms.append([a_gamma, b_gamma, 1.0, other_gamma])
             if self.is_first_day:
                 # win against virtual player ranked with gamma = 1.0
-                self._mov_game_terms.append([1.0, 0.0, 1.0, 1.0])
+                self._mov_game_terms.append([0.5, 0.5, 1.0, 1.0])
         return self._mov_game_terms
 
     def log_likelihood_second_derivative(self):
         result = 0.0
         for a, b, c, d in self.mov_game_terms():
             result += - (a * b) / ((a * self.gamma() + b) ** 2.0) + (c * d) / ((c * self.gamma() + d) ** 2.0)
-        return -1 * self.gamma() * result
+        return -1 * self.gamma() * result / (self.scale ** 2)
 
     def log_likelihood_derivative(self):
         tally = 0.0
         for a, b, c, d in self.mov_game_terms():
-            tally += a / (a * self.gamma() + b) - c / (c * self.gamma() + d)
-        return self.gamma() * tally
+            tally += a / (a * self.gamma() + b) 
+            tally -= c / (c * self.gamma() + d)
+        return self.gamma() * tally / self.scale
 
     def log_likelihood(self):
         tally = 0.0
